35f8b3ed6404af7867acb346c538f1e474e4a5fc
maesangaline <40840055+maesangaline@users.noreply.github.com>
2021-12-20 12:52:23 -0500

    Add files via upload

---
+++ chapter-4/Exercise13.py
@@ -0,0 +1,19 @@
+# Marcie Sangaline
+# CPT 156, Programming with Python
+# Chapter 4, Exercise 13
+
+
+# Input starting number of organisms
+startingnumber = int(input('Enter the starting number of organisms '))
+# Input the daily increase
+dailyincrease = int(input('Enter the daily population increase ' ))
+# Input the number of days left to multiply
+days = int(input('Enter the number of days left to multiply '))
+
+# Print the day approx and population, tabbed
+print('Day Approximate\t', 'Population')
+
+# For loop that uses the number of days then calculates the population increase by the percent of increase.
+for x in range(1, days + 1):
+    print(x, '\t', '\t', startingnumber)
+    startingnumber += startingnumber * dailyincrease / 100
---
+++ chapter-4/Exercise18.py
@@ -0,0 +1,19 @@
+# Marcie Sangaline
+# CPT 156, Programming with Python
+# Chapter 4, Exercise 18
+
+# Imports that turtle graphic
+import turtle
+
+# Sets move to 10  and the turtle head to 90 degrees
+move = 10
+turtle.setheading(90)
+
+# For loop to move turtle to the 11th loop
+for x in range(11):
+# For loop that moves turtle in forward then left 4 times before repeating
+    for y in range(4):
+        turtle.forward(move)
+        turtle.left(90)
+        move += 10
+
