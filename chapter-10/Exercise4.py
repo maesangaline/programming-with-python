3f1b0ffb333f0a6ea61f1145992a581efca1e8ab
maesangaline <40840055+maesangaline@users.noreply.github.com>
2021-12-20 13:04:46 -0500

    Add files via upload

---
+++ chapter-10/Exercise4.py
@@ -0,0 +1,59 @@
+# Marcie Sangaline
+# CPT 156 Programming with Python
+# Chapter 10, Exercise 4
+
+# Define the classes
+class employee:
+    emp_name = ''
+    id_num = ''
+    emp_dept = ''
+    emp_title = ''
+
+# Create an object from the employee class
+emp_obj_one = employee()
+
+# Assign values to obj one
+emp_obj_one.emp_name = 'Susan Meyers'
+emp_obj_one.id_num = '47899'
+emp_obj_one.emp_dept = 'Accounting'
+emp_obj_one.emp_title = 'Vice President'
+
+# Create another object
+emp_obj_two = employee()
+
+# Assign values to obj two
+emp_obj_two.emp_name = 'Mark Jones'
+emp_obj_two.id_num = '39119'
+emp_obj_two.emp_dept = 'IT'
+emp_obj_two.emp_title = 'Programmer'
+
+# Create another object
+emp_obj_three = employee()
+
+# Assign values to obj two
+emp_obj_three.emp_name = 'Joy Rogers'
+emp_obj_three.id_num = '81774'
+emp_obj_three.emp_dept = 'Manufacturing'
+emp_obj_three.emp_title = 'Engineer'
+
+# Print the employee details
+# Print employee 1
+print('Employee 1 details: ')
+print('Employee Name: ', emp_obj_one.emp_name)
+print('Employee ID Number: ', emp_obj_one.id_num)
+print('Employee Department: ', emp_obj_one.emp_dept)
+print('Employee Job Title: ', emp_obj_one.emp_title)
+
+# Print employee 2
+print('Employee 2 details: ')
+print('Employee Name: ', emp_obj_two.emp_name)
+print('Employee ID Number: ', emp_obj_two.id_num)
+print('Employee Department: ', emp_obj_two.emp_dept)
+print('Employee Job Title: ', emp_obj_two.emp_title)
+
+# Print employee 3
+print('Employee 2 details: ')
+print('Employee Name: ', emp_obj_three.emp_name)
+print('Employee ID Number: ', emp_obj_three.id_num)
+print('Employee Department: ', emp_obj_three.emp_dept)
+print('Employee Job Title: ', emp_obj_three.emp_title)
---
+++ chapter-10/Exercise5.py
@@ -0,0 +1,57 @@
+# Marcie Sangaline
+# CPT 156 Programming with Python
+# Chapter 10, Exercise 5
+
+# Define the classes
+class RetailItem:
+    item_num = ''
+    item_desc = ''
+    item_inven = ''
+    item_price = ''
+
+# Create an object from the retail item class
+item_obj_one = RetailItem()
+
+# Assign values to obj one
+item_obj_one.item_num = 'Item 1'
+item_obj_one.item_desc = 'Jacket'
+item_obj_one.item_inven = '12'
+item_obj_one.item_price = '59.95'
+
+# Create another object
+item_obj_two = RetailItem()
+
+# Assign values to obj two
+item_obj_two.item_num = 'Item 2'
+item_obj_two.item_desc = 'Designer Jeans'
+item_obj_two.item_inven = '40'
+item_obj_two.item_price = '34.95'
+
+# Create another object
+item_obj_three = RetailItem()
+
+# Assign values to obj three
+item_obj_three.item_num = 'Item 3'
+item_obj_three.item_desc = 'Shirt'
+item_obj_three.item_inven = '20'
+item_obj_three.item_price = '24.95'
+
+# Print the employee details
+# Print item 1
+print('Item 1 Details: ')
+print('Item Description: ', item_obj_one.item_desc)
+print('Item Units in Inventory: ', item_obj_one.item_inven)
+print('Item Pricet: ', item_obj_one.item_price)
+
+
+# Print item 2
+print('Item 2 Details: ')
+print('Item Description: ', item_obj_two.item_desc)
+print('Item Units in Inventory: ', item_obj_two.item_inven)
+print('Item Pricet: ', item_obj_two.item_price)
+
+# Print item 3
+print('Item 3 Details: ')
+print('Item Description: ', item_obj_three.item_desc)
+print('Item Units in Inventory: ', item_obj_three.item_inven)
+print('Item Pricet: ', item_obj_three.item_price)
